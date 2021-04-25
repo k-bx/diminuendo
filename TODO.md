19b14943c87891e07b2d254af99a0dd44a27694b
Kon Rybnikov <k-bx@k-bx.com>
2021-04-25 16:01:39 +0300

    Hardcode the device for now

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TODO (bottom to top)
+
+- [ ] **p2** plug device dynamically (check periodically or subscribe)
+- [ ] **p3** support multiple devices
+- [ ] **p2** select a USB device intelligently or let the user select it, currently my KORG is hardcoded
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("Error: {msg}")]
+    BL { msg: String },
+}
+
+pub fn bl_err(s: &str) -> AppError {
+    AppError::BL { msg: s.to_string() }
+}
+
 fn main() {
     let context = libusb::Context::new().unwrap();
+    let mut thedevice = None;
 
     for device in context.devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
@@ -9,7 +22,22 @@ fn main() {
             device.bus_number(),
             device.address(),
             device_desc.vendor_id(),
-            device_desc.product_id()
+            device_desc.product_id(),
         );
+
+        if device_desc.vendor_id() == 0944 && device_desc.product_id() == 0111 {
+            thedevice = Some(device);
+        }
     }
+
+    match thedevice {
+        None => {
+            panic!("Couldn't find a suitable USB-MIDI device");
+        }
+        Some(thedevice) => main2(thedevice).unwrap(),
+    }
+}
+
+fn main2(device: libusb::Device) -> Result<(), AppError> {
+    Ok(())
 }
